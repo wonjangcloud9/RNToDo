6476d64161729fe12f459c6fa306bab9cd69bd81
wonjangcloud9 <venuscwj@naver.com>
2023-06-05 07:36:47 +0900

    cal data

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import dayjs from "dayjs";
+import { getCalendarColumns } from "./src/util";
 
 export default function App() {
   const now = new Date();
   console.log(dayjs(now).format("YYYY-MM-DD HH:mm:ss"));
+  const x = getCalendarColumns(now);
+  console.log(x);
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
